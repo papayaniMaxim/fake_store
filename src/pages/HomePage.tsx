16b8d20bd797b3caf159bca0a7706dedc98c6d9f
papayaniMaxim <admin@MacBook-Pro-Admin.local>
2022-08-30 15:19:05 +0300

    nothing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,12 @@
 import classes from './HomePage.module.css'
 import ProductList from '../components/ProductList';
 import BurgerMenu from '../components/BurgerMenu';
-import { useState } from 'react';
-import Portal from '../components/Portal';
 function HomePage() {
-    const [open, setOpen] = useState(false)
-
-
+    
     return (
-        <div onClick={ ()=> setOpen(prev=>!prev) } className={classes.container}>
+        <div className={classes.container}>
             <BurgerMenu/>
             <ProductList />
-
         </div>
     )
     
