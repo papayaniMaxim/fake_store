fd6ca96a700e70f5c0f87062a4970a16c35d155d
papayaniMaxim <admin@MacBook-Pro-Admin.local>
2022-09-20 16:12:47 +0300

    debug cart icon

--- src/components/Cart/CardIcon/CardIcon.tsx
+++ src/components/Cart/CardIcon/CardIcon.tsx
@@ -16,7 +16,7 @@ function CardIcon() {
         whileHover={{ scale: 1.2 }}
         whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 400, damping: 17 }}
-            onMouseEnter={() => setModalOpen(() => true)}
+            onMouseEnter={() => cardItemsCount && setModalOpen(() => true)}
             onMouseLeave={() => setModalOpen(() => false)}
             onClick={() => setModalOpen(() => false)}>
             
