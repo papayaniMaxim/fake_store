68a7757470921ea994367b23eae8cba3573d6c73
papayaniMaxim <admin@MacBook-Pro-Admin.local>
2022-09-21 08:47:38 +0300

    debug pop-up cart window

--- src/components/Cart/CardIcon/CardIcon.tsx
+++ src/components/Cart/CardIcon/CardIcon.tsx
@@ -9,12 +9,13 @@ function CardIcon() {
     const [modalOpen, setModalOpen] = useState(false)
     const cardItemsCount = useSelector((state: State) => state.card.length)
 
+
     return (
         <motion.div
         whileHover={{ scale: 1.2 }}
         whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", stiffness: 400, damping: 17 }}
-            onMouseEnter={() => cardItemsCount && setModalOpen(() => true)}
+            onMouseEnter={() => cardItemsCount && (window.innerWidth>1000) && setModalOpen(() => true)}
             onMouseLeave={() => setModalOpen(() => false)}
             onClick={() => setModalOpen(() => false)}>
             
